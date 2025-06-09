ad1c273d2af66d5e28a9363a2a9fb4aab87048f6
Annareddyvineela12 <annareddyvineela12@gmail.com>
2025-06-09 10:07:26 +0530

    Add files via upload

---
+++ BFSMatrix.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class BFSMatrix{
+public void bfs(int[][] graph,int start){
+int n=graph.length;
+boolean[]visited=new boolaen[n];
+Queue<Integer>queue=new LinkedList<>();
+visited[start]=true;
+queue.offer(start);while(!queue.isEmpty()){
+int current=queue.poll();
+System.out.print(current+" ");
+for (int neighbor=0;neighbor<n;neighbor++){
+if (graph[current][neighbor]==1&&!visited[neighbor]){
+visited[neighbor]=true;
+queue.offer(neighbor);
+}
+}
+}
+}
+public static void main(String[] args){
+int[][] adjMatrix={{0,1,1,0},
+                   {1,0,0,1},
+				   {1,0,0,1},
+				   {0,1,1,0}};
+				   System.out.println("BFS starting from node 0:");
+				   bfs(adjMatrix,0);
+				   }
+				   }
